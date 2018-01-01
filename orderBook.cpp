012f0140d989500eb1eeec61cae3244139b38986
ngwd <ngwuedag@gmail.com>
2018-01-01 10:33:59 -0800

    Add files via upload

--- orderBook.cpp
+++ orderBook.cpp
@@ -2,23 +2,59 @@
 #include <string>
 #include <functional>
 #include <vector>
+#include <map>
 #include <queue>
+#include <set>
 
 enum class Side {
     buy = 0,
     sell = 1
 };
+enum class ordtype {
+    limit  = 'l',
+    market = 'm',
+    stop   = 's',
+    cancel = 'c'
+};
+enum class side {
+    buy  = 'b',
+    sell = 's',
+    none = 'n'
+};
 using ordid = int;
-using price = int;  // the price contains 2 fixed decimal, so we 
-                    // multiply all price with 100 to avoid float computing 
+using price = long;  // the price contains 2 fixed decimal, so we 
+                     // multiply all price with 100 to avoid float computing 
 struct compare_factor {price p;ordid o;};  // price, ordid
 using cmpfct = struct compare_factor;
+
+using volume = long;
+using type = char;
+
+class Order
+{
+public:
+    explicit Order(ordtype t = ordtype::limit, 
+                   side s    = side::buy,
+                   volume v  = 0L, 
+                   price  p  = 0L) noexcept:
+        _o(_id), _id(_id++), _t(t), _s(s), _v(v), _p(p){}
+    friend ostream& operator << (ostream& os, const Order& o);
+private:
+    static int _id;
+    const ordid _o = 1;             // order number 
+    const ordtype _t;
+    const side _s; 
+    const volume _v;
+    const price _p;
+    inline char* const _side2str() const  noexcept;
+    inline char* const _ordtype2str() const noexcept ;
+};
 
 struct OpenBooks 
 {
 public:
     OpenBooks();
-    std::vector<order>                _orders;
+    std::vector<Order>                _orders;
 
     std::function<bool(const cmpfct l, const cmpfct r)> _cmp[2];
 
@@ -36,6 +72,7 @@ public:
 };
 
 OpenBooks::OpenBooks() {
+    _orders.reserve(64);
     int buy = static_cast<int>(Side:buy);
     int sell= static_cast<int>(Side:sell);
     _cmp[buy] = [](cmpfct l, cmpfct r){
@@ -44,36 +81,73 @@ OpenBooks::OpenBooks() {
         return l.p>r.p || l.p==r.p && l.o>r.o;};
 }
 
-bool parseLine(const std::string & line, OpenBooks & ob)
+
+inline char* const Order::_side2str() const noexcept {
+    switch (_s){
+        case side::buy:  return "buy";
+        case side::sell: return "sell";
+    }
+    return "na";
+}
+
+inline char* const Order::_ordtype2str() const noexcept {
+    switch (_t){
+        case ordtype::limit: return "limit";
+        case ordtype::market:return "market";
+        case ordtype::stop:  return "stop";
+        case ordtype::cancel:return "cancel";
+    }
+    return "na";
+}
+
+ostream& operator<<(ostream& os, const Order& o) {
+    os<<o._ordtype2str()<<" "<<o._side2str()<<" "
+      <<o._v<<" "<<(long)(o._p/100) << "."
+      <<(int)(o._p%100);
+    return os;
+}
+
+void process(OpenBooks& exch, Order& od) {
+    exch._orders.push_back(od);
+}
+
+bool parseLine(const std::string & line, OpenBooks & exch)
 {
     char ordtyp[8] = {0x0};
-    char side[8] = {0x0};
-    long vol = 0L;
-    float price = 0.;
+    char side[8]   = {0x0};
+    long vol       = 0L;
+    long iprice    = 0L; // integer part of price
+    int  dprice    = 0L; // decimal part of price
     
-    const int narg = std::sscanf(line.c_str(), 
-             "%6s %4s %ld %f", ordtyp, side, &vol, &price);
-    return true;
-    if (narg != 4) 
-    {
+    int narg = std::sscanf(line.c_str(),"%6s %4s %ld %ld.%d", 
+             ordtyp, side, &vol, &iprice, &dprice);
+    if (narg != 5) {
         return false;
     }
-    const Order ord(ordtyp[0], side[0], vol, price); 
-    process(ob, ord);
+    Order od(static_cast<ordtype>(ordtyp[0]), 
+             static_cast<side>(side[0]), 
+             vol, 
+             iprice*100+dprice); 
+    process(exch, od);
     return true;
 }
 
 int main(int argc, char * argv[])
 {
-    OpenBooks ob;
+    OpenBooks exch;
     std::string line;
     while (std::getline(std::cin, line))
     {
-        if (!parseLine(line, ob))
+        if (!parseLine(line, exch))
         {
-            std::cerr << "Error while parsing line \"" << line << "\"" << std::endl;
+            std::cerr << "Error while parsing line \"" 
+                      << line << "\"" << std::endl;
             return 1;
         }
     }
+    // print
+    for(auto v : exch._orders) {
+        std::cout<<v<<std::endl;
+    } 
     return 0;
 }
