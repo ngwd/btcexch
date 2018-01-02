8fd05bad5d991f8ce2eab14742b9a46546fd4347
ngwd <ngwuedag@gmail.com>
2018-01-01 23:18:30 -0800

    Add files via upload

--- orderBook.cpp
+++ orderBook.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <iomanip>
 #include <functional>
 #include <vector>
 #include <map>
@@ -37,34 +39,34 @@ public:
                    side s    = side::buy,
                    volume v  = 0L, 
                    price  p  = 0L) noexcept:
-        _o(_id), _id(_id++), _t(t), _s(s), _v(v), _p(p){}
-    friend ostream& operator << (ostream& os, const Order& o);
+        _o(_id++), _t(t), _s(s), _v(v), _p(p){}
+    friend std::ostream& operator << (std::ostream& os, const Order& o);
 private:
-    static int _id;
-    const ordid _o = 1;             // order number 
+    static int   _id;
+    const ordid   _o;             // order number 
     const ordtype _t;
-    const side _s; 
-    const volume _v;
-    const price _p;
-    inline char* const _side2str() const  noexcept;
-    inline char* const _ordtype2str() const noexcept ;
+    const side    _s; 
+    const volume  _v;
+    const price   _p;
 };
-
+int Order::_id = 1;
 struct OpenBooks 
 {
 public:
     OpenBooks();
     std::vector<Order>                _orders;
 
-    std::function<bool(const cmpfct l, const cmpfct r)> _cmp[2];
+    // std::function<bool(const cmpfct l, const cmpfct r)> _cmp[2];
+    std::function<bool(const cmpfct l, const cmpfct r)> _cmp_s; 
+    std::function<bool(const cmpfct l, const cmpfct r)> _cmp_b; 
 
-    int buy = static_cast<int>(Side:buy); 
-    int sell= static_cast<int>(Side:sell);
+    int buy = static_cast<int>(Side::buy); 
+    int sell= static_cast<int>(Side::sell);
 
     std::priority_queue<cmpfct, std::vector<cmpfct>, 
-        decltype(_cmp[buy])>  _limit_order(_cmp[buy]);
+        decltype(_cmp_b)>  _limit_order_b;
     std::priority_queue<cmpfct, std::vector<cmpfct>, 
-        decltype(_cmp[sell])> _limit_order(_cmp[sell]);
+        decltype(_cmp_s)> _limit_order_s;
 
     std::map<price, ordid>            _stop_order  [2];
     std::set<ordid>                   _cancel_order[2];
@@ -73,37 +75,40 @@ public:
 
 OpenBooks::OpenBooks() {
     _orders.reserve(64);
-    int buy = static_cast<int>(Side:buy);
-    int sell= static_cast<int>(Side:sell);
-    _cmp[buy] = [](cmpfct l, cmpfct r){
+    int buy = static_cast<int>(Side::buy);
+    int sell= static_cast<int>(Side::sell);
+
+    //_cmp[buy]  = [](cmpfct l, cmpfct r){
+    _cmp_b = [](cmpfct l, cmpfct r){
         return l.p<r.p || l.p==r.p && l.o>r.o;};
-    _cmp[sell] = [](cmpfct l, cmpfct r){ 
+    //_cmp[sell] = [](cmpfct l, cmpfct r){ 
+    _cmp_s = [](cmpfct l, cmpfct r){ 
         return l.p>r.p || l.p==r.p && l.o>r.o;};
 }
 
 
-inline char* const Order::_side2str() const noexcept {
-    switch (_s){
-        case side::buy:  return "buy";
-        case side::sell: return "sell";
+inline char* const side2str(side s) noexcept {
+    switch (s){
+        case side::buy:  return (char*)"buy";
+        case side::sell: return (char*)"sell";
     }
-    return "na";
+    return (char*)"na";
 }
 
-inline char* const Order::_ordtype2str() const noexcept {
-    switch (_t){
-        case ordtype::limit: return "limit";
-        case ordtype::market:return "market";
-        case ordtype::stop:  return "stop";
-        case ordtype::cancel:return "cancel";
+inline char* const ordtype2str(ordtype t) noexcept {
+    switch (t){
+        case ordtype::limit: return (char*)"limit";
+        case ordtype::market:return (char*)"market";
+        case ordtype::stop:  return (char*)"stop";
+        case ordtype::cancel:return (char*)"cancel";
     }
-    return "na";
+    return (char*)"na";
 }
 
-ostream& operator<<(ostream& os, const Order& o) {
-    os<<o._ordtype2str()<<" "<<o._side2str()<<" "
+std::ostream& operator<<(std::ostream& os, const Order& o) {
+    os<<ordtype2str(o._t)<<" "<<side2str(o._s)<<" "
       <<o._v<<" "<<(long)(o._p/100) << "."
-      <<(int)(o._p%100);
+      << std::setfill('0') << std::setw(2) << (int)(o._p%100);
     return os;
 }
 
@@ -114,18 +119,18 @@ void process(OpenBooks& exch, Order& od) {
 bool parseLine(const std::string & line, OpenBooks & exch)
 {
     char ordtyp[8] = {0x0};
-    char side[8]   = {0x0};
+    char sidestr[8]   = {0x0};
     long vol       = 0L;
     long iprice    = 0L; // integer part of price
     int  dprice    = 0L; // decimal part of price
     
     int narg = std::sscanf(line.c_str(),"%6s %4s %ld %ld.%d", 
-             ordtyp, side, &vol, &iprice, &dprice);
+             ordtyp, sidestr, &vol, &iprice, &dprice);
     if (narg != 5) {
         return false;
     }
     Order od(static_cast<ordtype>(ordtyp[0]), 
-             static_cast<side>(side[0]), 
+             static_cast<side>(sidestr[0]), 
              vol, 
              iprice*100+dprice); 
     process(exch, od);
@@ -135,8 +140,11 @@ bool parseLine(const std::string & line, OpenBooks & exch)
 int main(int argc, char * argv[])
 {
     OpenBooks exch;
-    std::string line;
-    while (std::getline(std::cin, line))
+    std::ifstream inf("odbk_test1");
+    char line[100];
+    //std::string line;
+    // while (std::getline(std::cin, line))
+    while(!inf.getline(line, 100).eof())
     {
         if (!parseLine(line, exch))
         {
@@ -149,5 +157,6 @@ int main(int argc, char * argv[])
     for(auto v : exch._orders) {
         std::cout<<v<<std::endl;
     } 
+    
     return 0;
 }
